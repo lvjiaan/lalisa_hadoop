2f1e879eddb7635d913564abfa255c53ca736b0d
lvja <28788504@qq.com>
2021-02-05 14:10:57 +0800

    spring cloud

---
+++ web/src/main/java/cn/lvjaweb/web/WebApplication.java
@@ -0,0 +1,18 @@
+package cn.lvjaweb.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan(basePackages = {"cn.lvjaserver"})
+@ComponentScan(basePackages = {"cn.lvjaweb"})
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+
+}
